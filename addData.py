b412bea352349c714f51bb455e9578455ff8e4bb
carlb14 <166364118+carlb14@users.noreply.github.com>
2024-06-11 15:36:59 +0800

    Update addData.py

--- addData.py
+++ addData.py
@@ -36,29 +36,43 @@ def choose_action():
     else:
         print('\nInvalid option\n')
         
+def check_existence(collection, field, value):
+    # Check if any document in the collection has the specified field with the given value
+    return collection.find_one({field: value}) is not None
+
 def add_company():
     while True:
-        
-        company_add = input('Enter the name of the company: ')
-        add = {"name": company_add}
-        company_col.insert_one(add)
+        company_add = input('Enter the name of the company (or type "quit" to exit): ')
         if company_add.lower() == 'quit':
             choose_action()
+        if check_existence(company_col, "name", company_add):
+            print("The company already exists.")
+        else:
+            add = {"name": company_add}
+            company_col.insert_one(add)
+
 def add_address():
     while True:
-        address_add = input('Enter the name of the company: ')
-        add = {"name": address_add}
-        address_col.insert_one(add)
-        if add_address().lower() == 'quit':
-            choose_action()    
+        address_add = input('Enter the name of the address (or type "quit" to exit): ')
+        if address_add.lower() == 'quit':
+            choose_action()
+        if check_existence(address_col, "name", address_add):
+            print("The address already exists.")
+        else:
+            add = {"name": address_add}
+            address_col.insert_one(add)
 
 def add_description():
     while True:
-        description_add = input('Enter the name of the company: ')
-        add = {"name": description_add}
-        description_col.insert_one(add)        
-        if add_description().lower() == 'quit':
-            choose_action()  
+        description_add = input('Enter the name of the description (or type "quit" to exit): ')
+        if description_add.lower() == 'quit':
+            choose_action()
+        if check_existence(description_col, "name", description_add):
+            print("The description already exists.")
+        else:
+            add = {"name": description_add}
+            description_col.insert_one(add)
+
 if __name__ == "__main__":
 
         choose_action()
